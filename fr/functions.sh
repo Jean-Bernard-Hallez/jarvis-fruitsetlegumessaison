0038e95c62abf4675a683c48573eb8cd98a4ac7b
Hallez <jbhallez@aol.com>
2017-05-31 19:31:50 +0200

    Ce plugin pour Jarvis permet de trouver à quel moment on peut manger tel ou tel fruit / légume !!
    you: à quelle saison on mange des pommes
    jarvis: Voici les 3 mois que j'ai trouvé pour pomme: août, septembre,
    octobre.
    
    you: quels sont les fruits de saison
    jarvis: Voici les fruits de saison du mois de mai: Cerise, Fraise,
    Pamplemousse, Rhubarbe
    
    you: quels sont les légumes de saison
    jarvis: Voici les légumes de saison du mois de mai: Ail, Artichaut,
    Asperge, Aubergine, Betterave, Blette, Carotte, Céleri, Chou-fleur,
    Concombre, Courgette, Épinard, Laitue, Navet, Oignon, Petit pois, Pomme
    de terre, primeur, Radis, Tomate.

--- fr/functions.sh
+++ fr/functions.sh
@@ -8,14 +8,37 @@ jv_pg_fruits_saison() {
 chemin=${PWD}"/plugins_installed/jarvis-fruitsetlegumessaison/fruitslegumes.txt"
 onest_saison_now=`date +%B`
 ligne_num_fruitslegumes=`grep -i "$onest_saison_now - FRUITS" $chemin | sed -e "s/$onest_saison_now - FRUITS: //g"`
-say "Voici les fruits de saison du mois de $onest_saison_now: $ligne_num_fruitslegumes"
+ligne_num_fruitslegumes_max=`echo "$ligne_num_fruitslegumes" | wc -c`
+ligne_num_fruitslegumes_max1=`echo "$ligne_num_fruitslegumes" | grep -n ',' | wc -w`
+ligne_num_fruitslegumes="Voici les $ligne_num_fruitslegumes_max1 fruits de saison du mois de $onest_saison_now: $ligne_num_fruitslegumes"
+if [[ "$ligne_num_fruitslegumes_max" -gt "150" ]]; then 
+ligne_num_fruitslegumes_2=$(( $ligne_num_fruitslegumes_max1 / 2 ))
+ligne_num_fruitslegumes1=`echo "$ligne_num_fruitslegumes" | cut -d"," -f1-$ligne_num_fruitslegumes_2`
+ligne_num_fruitslegumes2=`echo "$ligne_num_fruitslegumes" | cut -d"," -f$(( $ligne_num_fruitslegumes_2 + 1 ))-`
+say "$ligne_num_fruitslegumes1"
+say "$ligne_num_fruitslegumes2"
+else
+say "$ligne_num_fruitslegumes"
+fi
+
 }
 
 jv_pg_legumes_saison() {
 chemin=${PWD}"/plugins_installed/jarvis-fruitsetlegumessaison/fruitslegumes.txt"
 onest_saison_now=`date +%B`
 ligne_num_fruitslegumes=`grep -i "$onest_saison_now - LÉGUMES" $chemin | sed -e "s/$onest_saison_now - LÉGUMES: //g"`
-say "Voici les légumes de saison du mois de $onest_saison_now: $ligne_num_fruitslegumes"
+ligne_num_fruitslegumes_max=`echo "$ligne_num_fruitslegumes" | wc -c`
+ligne_num_fruitslegumes_max1=`echo "$ligne_num_fruitslegumes" | grep -n ',' | wc -w`
+ligne_num_fruitslegumes="Voici les $ligne_num_fruitslegumes_max1 légumes de saison du mois de $onest_saison_now: $ligne_num_fruitslegumes"
+if [[ "$ligne_num_fruitslegumes_max" -gt "150" ]]; then 
+ligne_num_fruitslegumes_2=$(( $ligne_num_fruitslegumes_max1 / 2 ))
+ligne_num_fruitslegumes1=`echo "$ligne_num_fruitslegumes" | cut -d"," -f1-$ligne_num_fruitslegumes_2`
+ligne_num_fruitslegumes2=`echo "$ligne_num_fruitslegumes" | cut -d"," -f$(( $ligne_num_fruitslegumes_2 + 1 ))-`
+say "$ligne_num_fruitslegumes1"
+say "$ligne_num_fruitslegumes2"
+else
+say "$ligne_num_fruitslegumes"
+fi
 }
 
 jv_pg_quel_saison() {
