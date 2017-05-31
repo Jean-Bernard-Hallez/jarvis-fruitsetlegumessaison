579bcedaff3390ca93568e75f550f8363e873f6e
Hallez <jbhallez@aol.com>
2017-05-31 18:33:58 +0200

    Ce plugin pour Jarvis permet de trouver à quel moment on peut manger tel ou tel fruit / légume !!
    you: à quelle saison on mange des pommes
    jarvis: Voici les 3 mois que j'ai trouvé pour pomme: août, septembre,
    octobre.
    
    you: quels sont les fruits de saison
    jarvis: Voici les fruits de saison du mois de mai: Cerise, Fraise,
    Pamplemousse, Rhubarbe
    
    you: quels sont les légumes de saison
    jarvis: Voici les légumes de saison du mois de mai: Ail, Artichaut,
    Asperge, Aubergine, Betterave, Blette, Carotte, Céleri, Chou-fleur,
    Concombre, Courgette, Épinard, Laitue, Navet, Oignon, Petit pois, Pomme
    de terre, primeur, Radis, Tomate.

---
+++ README.md
@@ -0,0 +1,23 @@
+## Description
+Ce plugin pour Jarvis permet de trouver à quel moment on peut manger tel ou tel fruit / légume !!
+
+## Languages
+
+* Français
+
+## Usage
+```
+you: à quelle saison on mange des pommes
+jarvis: Voici les 3 mois que j'ai trouvé pour pomme: août, septembre, octobre.
+
+you: quels sont les fruits de saison
+jarvis: Voici les fruits de saison du mois de mai: Cerise, Fraise, Pamplemousse, Rhubarbe
+
+you: quels sont les légumes de saison
+jarvis: Voici les légumes de saison du mois de mai: Ail, Artichaut, Asperge, Aubergine, Betterave, Blette, Carotte, Céleri, Chou-fleur, Concombre, Courgette, Épinard, Laitue, Navet, Oignon, Petit pois, Pomme de terre, primeur, Radis, Tomate.
+
+```
+
+## Author
+[Jean-Bernard Hallez](https://github.com/Jean-Bernard-Hallez/jarvis-fruitsetlegumessaison)
+
---
+++ fr/functions.sh
@@ -0,0 +1,70 @@
+# /bin/bash
+
+# *FRUIT*SAISON*==jv_pg_fruits_saison
+# *LEGUME*SAISON*==jv_pg_legumes_saison
+# *QUEL*SAISON*==jv_pg_quel_saison
+
+jv_pg_fruits_saison() {
+chemin=${PWD}"/plugins_installed/jarvis-fruitsetlegumessaison/fruitslegumes.txt"
+onest_saison_now=`date +%B`
+ligne_num_fruitslegumes=`grep -i "$onest_saison_now - FRUITS" $chemin | sed -e "s/$onest_saison_now - FRUITS: //g"`
+say "Voici les fruits de saison du mois de $onest_saison_now: $ligne_num_fruitslegumes"
+}
+
+jv_pg_legumes_saison() {
+chemin=${PWD}"/plugins_installed/jarvis-fruitsetlegumessaison/fruitslegumes.txt"
+onest_saison_now=`date +%B`
+ligne_num_fruitslegumes=`grep -i "$onest_saison_now - LÉGUMES" $chemin | sed -e "s/$onest_saison_now - LÉGUMES: //g"`
+say "Voici les légumes de saison du mois de $onest_saison_now: $ligne_num_fruitslegumes"
+}
+
+jv_pg_quel_saison() {
+chemin=${PWD}"/plugins_installed/jarvis-fruitsetlegumessaison/fruitslegumes.txt"
+chemin=${PWD}"/plugins_installed/jarvis-fruitsetlegumessaison/fruitslegumes.txt"
+order_fruitslegumes=`echo $order | sed 's/.* //'`
+order_fruitslegumes_S_P=`echo $order_fruitslegumes | tail -c 2`
+if [[ "$order_fruitslegumes_S_P" == "s" ]]; then
+order_fruitslegumes=`echo $order | sed 's/.* //' | sed "s/.$//"`
+fi
+order_fruitslegumes="$order_fruitslegumes,"
+ligne_num_fruitslegumes_total=`grep -i -n $order_fruitslegumes $chemin | cut -d" " -f1 | wc -w`
+
+ligne_num_fruitslegumes_total1="1"
+fruitslegumes_ok=""
+if [[ "$ligne_num_fruitslegumes_total" == "0" ]]; then 
+say "désolé, je n'ai touvé aucun mois correspondants à $order"
+return
+fi
+regarde_traitement_fruitslegumes
+
+if [[ "$ligne_num_fruitslegumes_total" == "12" ]]; then 
+say "On trouve toute l'année $order"
+return
+fi
+
+fruitslegumes_ok=`echo $fruitslegumes_ok | sed "s/.$//"`
+fruitslegumes_ok="$fruitslegumes_ok."
+
+order_fruitslegumes=`echo $order | sed 's/.* //' | sed "s/.$//"`
+
+if [[ "$ligne_num_fruitslegumes_total" == "1" ]]; then 
+say "Le mois que j'ai trouvé pour $order_fruitslegumes est en: $fruitslegumes_ok"
+else
+say "Voici les $ligne_num_fruitslegumes_total mois que j'ai trouvé pour $order_fruitslegumes: $fruitslegumes_ok"
+fi
+}
+
+regarde_traitement_fruitslegumes() {
+if [[ "$ligne_num_fruitslegumes_total1" -lt "$(( $ligne_num_fruitslegumes_total + 1 ))" ]]; then
+# ligne_num_fruitslegumes=`grep -i $symptome_fruitslegumes $chemin | cut -d: -f1 | sed -n $ligne_num_fruitslegumes_total1\p | cut -d"-" -f1`
+ligne_num_fruitslegumes=`grep -i "$order_fruitslegumes" $chemin | cut -d" " -f1 | sed -n $ligne_num_fruitslegumes_total1\p`
+
+if [[ "$jv_pg_fruitslegumesSMS" == "" ]]; then 
+fruitslegumes_ok="$fruitslegumes_ok $ligne_num_fruitslegumes,"
+fi
+
+ligne_num_fruitslegumes_total1=$(( $ligne_num_fruitslegumes_total1 + 1 ))
+regarde_traitement_fruitslegumes
+fi
+
+}
